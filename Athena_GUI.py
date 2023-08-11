d6041f00fd9b615ef9b9ba6f5a51ab6d8f368965
Oleg Budaev <t0r1n88@mail.ru>
2023-08-11 09:01:04 +0800

    разделил функционал по отдельным модулям

--- Athena_GUI.py
+++ Athena_GUI.py
@@ -14,7 +14,15 @@ import warnings
 
 warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
+# объединение файлов ОПК по ДФО
+from union_opk_dfo import select_folder_data_opk_dfo
+from union_opk_dfo import select_end_folder_opk_dfo
+from union_opk_dfo import processing_data_opk_dfo
 
+# Объедениение файлов по демоэкзамену
+from union_demo_exam import select_folder_data_demo_exam
+from union_demo_exam import select_end_folder_demo_exam
+from union_demo_exam import processing_data_demo_exam
 
 
 def resource_path(relative_path):
@@ -29,86 +37,9 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-def select_folder_data():
-    """
-    Функция для выбора папки c данными
-    :return:
-    """
-    global path_folder_data
-    path_folder_data = filedialog.askdirectory()
-
-
-def select_end_folder():
-    """
-    Функция для выбора конечной папки куда будут складываться итоговые файлы
-    :return:
-    """
-    global path_to_end_folder
-    path_to_end_folder = filedialog.askdirectory()
-
-
-def processing_data():
-    """
-    Фугкция для объединения данных из всех трех листов из файлов от каждого региона
-    :return:
-    """
-    try:
-        form1_df = pd.DataFrame(columns=range(22))
-        form2_df = pd.DataFrame(columns=range(11))
-        form3_df = pd.DataFrame(columns=range(8))
-
-        for file in os.listdir(path_folder_data):
-            if (file.endswith('.xlsx') and not file.startswith('~$')):
-                print(file)
-                # обрабатываем первый лист
-                temp_df = pd.read_excel(f'{path_folder_data}/{file}', sheet_name=0, dtype={'гр.2': str}, skiprows=2,
-                                        usecols='A:V')
-                temp_df.columns = range(22)
-                temp_df.dropna(thresh=4, inplace=True)
-                form1_df = pd.concat([form1_df, temp_df], ignore_index=True)
-
-                # обрабатываем второй лист
-                temp_df = pd.read_excel(f'{path_folder_data}/{file}', sheet_name=1, dtype={'гр.2': str}, skiprows=2,
-                                        usecols='A:K')
-                temp_df.columns = range(11)
-                temp_df.dropna(thresh=4, inplace=True)
-                form2_df = pd.concat([form2_df, temp_df], ignore_index=True)
-
-                # обрабатываем третий лист
-                temp_df = pd.read_excel(f'{path_folder_data}/{file}', sheet_name=2, dtype={'гр.2': str}, skiprows=2,
-                                        usecols='A:H')
-                temp_df.columns = range(8)
-                temp_df.dropna(thresh=3, inplace=True)
-                form3_df = pd.concat([form3_df, temp_df], ignore_index=True)
-
-        # генерируем текущее время
-        t = time.localtime()
-        current_time = time.strftime('%H_%M_%S', t)
-
-        with pd.ExcelWriter(f'{path_to_end_folder}/Общий файл от {current_time}.xlsx') as writer:
-            form1_df.to_excel(writer, sheet_name='Форма по мониторингу', index=False)
-            form2_df.to_excel(writer, sheet_name='Форма по принимаемым мерам', index=False)
-            form3_df.to_excel(writer, sheet_name='Форма по социальной поддежке', index=False)
-    except NameError:
-            messagebox.showerror('Морриган Объединение данных по ОПК ver 1.0',
-                                 f'Выберите файлы с данными и папку куда будет генерироваться файл')
-    except KeyError as e:
-        messagebox.showerror('Морриган Объединение данных по ОПК ver 1.0',
-                             f'Не найдено значение {e.args}')
-    except FileNotFoundError:
-        messagebox.showerror('Морриган Объединение данных по ОПК ver 1.0',
-                             f'Перенесите файлы которые вы хотите обработать в корень диска. Проблема может быть\n '
-                             f'в слишком длинном пути к обрабатываемым файлам')
-    except PermissionError as e:
-        messagebox.showerror('Морриган Объединение данных по ОПК ver 1.0',
-                             f'Закройте открытые файлы Excel {e.args}')
-    else:
-        messagebox.showinfo('Морриган Объединение данных по ОПК ver 1.0',
-                            'Данные успешно обработаны.')
-
 if __name__ == '__main__':
     window = Tk()
-    window.title('Морриган Объединение данных по ОПК ver 1.0')
+    window.title('Афина ver 1.0')
     window.geometry('700x860')
     window.resizable(False, False)
 
@@ -117,41 +48,42 @@ if __name__ == '__main__':
     tab_control = ttk.Notebook(window)
 
     # Создаем вкладку обработки данных для Приложения 6
-    tab_union_1 = ttk.Frame(tab_control)
-    tab_control.add(tab_union_1, text='Скрипт №1')
+    tab_opk_dfo = ttk.Frame(tab_control)
+    tab_control.add(tab_opk_dfo, text='Скрипт ОПК ДФО')
     tab_control.pack(expand=1, fill='both')
     # Добавляем виджеты на вкладку Создание образовательных программ
     # Создаем метку для описания назначения программы
-    lbl_hello = Label(tab_union_1,
-                      text='Центр опережающей профессиональной подготовки Республики Бурятия')
-    lbl_hello.grid(column=0, row=0, padx=10, pady=25)
+    lbl_hello_opk_dfo = Label(tab_opk_dfo,
+                              text='Центр опережающей профессиональной подготовки Республики Бурятия\n'
+                                   'Обработка файлов трудоустройства ОПК от регионов ДФО ')
+    lbl_hello_opk_dfo.grid(column=0, row=0, padx=10, pady=25)
 
     # Картинка
-    path_to_img = resource_path('logo.png')
+    path_to_img_opk_dfo = resource_path('logo.png')
 
-    img = PhotoImage(file=path_to_img)
-    Label(tab_union_1,
-          image=img
+    img_opk_dfo = PhotoImage(file=path_to_img_opk_dfo)
+    Label(tab_opk_dfo,
+          image=img_opk_dfo
           ).grid(column=1, row=0, padx=10, pady=25)
 
     # Создаем кнопку Выбрать файл с данными
-    btn_choose_data = Button(tab_union_1, text='1) Выберите папку с данными', font=('Arial Bold', 20),
-                             command=select_folder_data
-                             )
-    btn_choose_data.grid(column=0, row=2, padx=10, pady=10)
+    btn_choose_data_opk_dfo = Button(tab_opk_dfo, text='1) Выберите папку с данными', font=('Arial Bold', 20),
+                                     command=select_folder_data_opk_dfo
+                                     )
+    btn_choose_data_opk_dfo.grid(column=0, row=2, padx=10, pady=10)
 
     # Создаем кнопку для выбора папки куда будут генерироваться файлы
 
-    btn_choose_end_folder = Button(tab_union_1, text='2) Выберите конечную папку', font=('Arial Bold', 20),
-                                   command=select_end_folder
-                                   )
-    btn_choose_end_folder.grid(column=0, row=3, padx=10, pady=10)
+    btn_choose_end_folder_opk_dfo = Button(tab_opk_dfo, text='2) Выберите конечную папку', font=('Arial Bold', 20),
+                                           command=select_end_folder_opk_dfo
+                                           )
+    btn_choose_end_folder_opk_dfo.grid(column=0, row=3, padx=10, pady=10)
 
     # Создаем кнопку обработки данных
 
-    btn_proccessing_data = Button(tab_union_1, text='3) Обработать данные', font=('Arial Bold', 20),
-                                  command=processing_data
-                                  )
-    btn_proccessing_data.grid(column=0, row=4, padx=10, pady=10)
+    btn_proccessing_data_opk_dfo = Button(tab_opk_dfo, text='3) Обработать данные', font=('Arial Bold', 20),
+                                          command=processing_data_opk_dfo
+                                          )
+    btn_proccessing_data_opk_dfo.grid(column=0, row=4, padx=10, pady=10)
 
     window.mainloop()
---
+++ union_opk_dfo.py
@@ -0,0 +1,91 @@
+"""
+Функции для объединения файлов по трудоустройству ОПК в ДФо
+"""
+import pandas as pd
+import os
+from tkinter import filedialog
+from tkinter import messagebox
+import time
+pd.options.mode.chained_assignment = None  # default='warn'
+import warnings
+
+warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
+
+
+def select_folder_data_opk_dfo():
+    """
+    Функция для выбора папки c данными от цопп по опк
+    :return:
+    """
+    global path_folder_data_opk_dfo
+    path_folder_data_opk_dfo = filedialog.askdirectory()
+
+
+def select_end_folder_opk_dfo():
+    """
+    Функция для выбора конечной папки куда будут складываться итоговые файлы
+    :return:
+    """
+    global path_to_end_folder_opk_dfo
+    path_to_end_folder_opk_dfo = filedialog.askdirectory()
+
+
+def processing_data_opk_dfo():
+    """
+    Фугкция для объединения данных из всех трех листов из файлов от каждого региона ДФО
+    :return:
+    """
+    try:
+        form1_df = pd.DataFrame(columns=range(22))
+        form2_df = pd.DataFrame(columns=range(11))
+        form3_df = pd.DataFrame(columns=range(8))
+
+        for file in os.listdir(path_folder_data_opk_dfo):
+            if (file.endswith('.xlsx') and not file.startswith('~$')):
+                print(file)
+                # обрабатываем первый лист
+                temp_df = pd.read_excel(f'{path_folder_data_opk_dfo}/{file}', sheet_name=0, dtype={'гр.2': str}, skiprows=2,
+                                        usecols='A:V')
+                temp_df.columns = range(22)
+                temp_df.dropna(thresh=4, inplace=True)
+                form1_df = pd.concat([form1_df, temp_df], ignore_index=True)
+
+                # обрабатываем второй лист
+                temp_df = pd.read_excel(f'{path_folder_data_opk_dfo}/{file}', sheet_name=1, dtype={'гр.2': str}, skiprows=2,
+                                        usecols='A:K')
+                temp_df.columns = range(11)
+                temp_df.dropna(thresh=4, inplace=True)
+                form2_df = pd.concat([form2_df, temp_df], ignore_index=True)
+
+                # обрабатываем третий лист
+                temp_df = pd.read_excel(f'{path_folder_data_opk_dfo}/{file}', sheet_name=2, dtype={'гр.2': str}, skiprows=2,
+                                        usecols='A:H')
+                temp_df.columns = range(8)
+                temp_df.dropna(thresh=3, inplace=True)
+                form3_df = pd.concat([form3_df, temp_df], ignore_index=True)
+
+        # генерируем текущее время
+        t = time.localtime()
+        current_time = time.strftime('%H_%M_%S', t)
+
+        with pd.ExcelWriter(f'{path_to_end_folder_opk_dfo}/Общий файл от {current_time}.xlsx') as writer:
+            form1_df.to_excel(writer, sheet_name='Форма по мониторингу', index=False)
+            form2_df.to_excel(writer, sheet_name='Форма по принимаемым мерам', index=False)
+            form3_df.to_excel(writer, sheet_name='Форма по социальной поддежке', index=False)
+    except NameError:
+            messagebox.showerror('Афина',
+                                 f'Выберите файлы с данными и папку куда будет генерироваться файл')
+    except KeyError as e:
+        messagebox.showerror('Афина',
+                             f'Не найдено значение {e.args}')
+    except FileNotFoundError:
+        messagebox.showerror('Афина',
+                             f'Перенесите файлы которые вы хотите обработать в корень диска. Проблема может быть\n '
+                             f'в слишком длинном пути к обрабатываемым файлам')
+    except PermissionError as e:
+        messagebox.showerror('Афина',
+                             f'Закройте открытые файлы Excel {e.args}')
+    else:
+        messagebox.showinfo('Афина',
+                            'Данные успешно обработаны.')
